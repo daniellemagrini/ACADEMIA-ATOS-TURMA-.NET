a2b58c9e4041a3982e6759d30018ab958fbdc7d4
Danielle Magrini <danielle.magrini@hotmail.com>
2022-04-29 14:52:48 -0300

    Enviado em 29/04/2022 às 14:52h

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * 8) Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. 
+ *    A fórmula da conversão é F=(9*C+160)/5.
+**/
+
+Console.WriteLine("Exercício 8 - Aula 2");
+Console.WriteLine("Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit.\n" +
+    "A fórmula da conversão é F = (9 * C + 160) / 5.");
+
+Console.WriteLine("Digite a temperatura que deseja converter");
+float temp_celsius  = float.Parse(Console.ReadLine());
+
+float temp_fahrenheit = (9 * temp_celsius + 160) / 5;
+
+Console.WriteLine("A temperatura correspondente, em fahrenheit é: " + temp_fahrenheit);
