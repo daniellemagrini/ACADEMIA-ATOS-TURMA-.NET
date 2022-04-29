85bc8451523bbd6fa351b8e0597cbfae7c440e4b
Danielle Magrini <danielle.magrini@hotmail.com>
2022-04-28 21:00:25 -0300

    Enviado em 28/04/2022 às 21:00h

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * 1) Escreva um programa em C# e no Visual Studio
+ * para calcular a média aritmética entre dois números quaisquer.
+**/
+
+Console.WriteLine("Exercício 4 - Aula 2");
+Console.WriteLine("Faça um programa em C# e no Visual Studio que receba a data na forma DDMMAAAA\n" +
+    "e imprima na forma AAAAMMDD e AAMMDD. Note que o dia, o mês e o ano devem ser lidos\n" +
+    "em variáveis diferentes.");
+
+Console.WriteLine("Qual sua data de nascimento?");
+Console.WriteLine("Digite o dia em que nasceu");
+int dia = int.Parse(Console.ReadLine());
+Console.WriteLine("Agora digite o numero do mês em que nasceu");
+int mes = int.Parse(Console.ReadLine());
+Console.WriteLine("E por fim, digite o ano completo em que nasceu");
+int ano = int.Parse(Console.ReadLine());
+
+DateOnly data_nasc = new DateOnly(ano, mes, dia); //Transforma o que foi digitado em data
+string data_nasc1 = data_nasc.ToString("yyyy/MM/dd");
+string data_nasc2 = data_nasc.ToString("yy/MM/dd");
+
+Console.WriteLine("Sua data de nascimento no formato AAAAMMDD é:" + data_nasc1);
+Console.WriteLine("Sua data de nascimento no formato AAMMDD é:" + data_nasc2);
